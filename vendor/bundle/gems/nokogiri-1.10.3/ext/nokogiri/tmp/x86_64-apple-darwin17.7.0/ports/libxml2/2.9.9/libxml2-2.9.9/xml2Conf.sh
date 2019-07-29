ab58cf9e6063398941f37149363ea483e5f61f25
waspryo <wasp2123321@gmail.com>
2019-07-29 16:37:38 +0900

    first commit

---
+++ vendor/bundle/gems/nokogiri-1.10.3/ext/nokogiri/tmp/x86_64-apple-darwin17.7.0/ports/libxml2/2.9.9/libxml2-2.9.9/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/wasp/Desktop/My_Project/Ruby on Rails dev-course/Rails_projects/test_app/vendor/bundle/gems/nokogiri-1.10.3/ports/x86_64-apple-darwin17.7.0/libxml2/2.9.9/lib"
+XML2_LIBS="-lxml2 -lz -L/usr/local/Cellar/xz/5.2.4/lib -llzma -lpthread  -liconv  -lm "
+XML2_INCLUDEDIR="-I/Users/wasp/Desktop/My_Project/Ruby on Rails dev-course/Rails_projects/test_app/vendor/bundle/gems/nokogiri-1.10.3/ports/x86_64-apple-darwin17.7.0/libxml2/2.9.9/include/libxml2"
+MODULE_VERSION="xml2-2.9.9"
+
