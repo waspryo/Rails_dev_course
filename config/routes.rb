315d2f97bd2d83d93206770f0a0dfebd6ebf7c9c
waspryo <wasp2123321@gmail.com>
2019-07-31 16:02:07 +0900

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 end
