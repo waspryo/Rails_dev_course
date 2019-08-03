1dabe2c9620658f4e053cc01fc14856d78710dd2
waspryo <wasp2123321@gmail.com>
2019-08-03 15:21:28 +0900

    delete scaffold

--- db/migrate/20190803025156_create_articles.rb
+++ db/migrate/20190803025156_create_articles.rb
@@ -2,7 +2,6 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_08_03_035105) do
-=======
-ActiveRecord::Schema.define(version: 2019_08_03_025156) do
->>>>>>> e99218910c8f6a2856ef7b9f758b60313e3cfa2a
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-<<<<<<< HEAD
     t.datetime "created_at"
     t.datetime "updated_at"
-=======
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
->>>>>>> e99218910c8f6a2856ef7b9f758b60313e3cfa2a
   end
 
 end
