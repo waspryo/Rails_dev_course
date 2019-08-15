d9d821776d8f8a2371f95fcd08dddf3ba648c5d3
waspryo <wasp2123321@gmail.com>
2019-08-15 23:01:45 +0900

    created validation error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
             redirect_to article_path(@article)
