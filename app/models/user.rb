86ecec7aeb31e614224322de113f214adcf28a2f
waspryo <wasp2123321@gmail.com>
2019-08-28 15:02:37 +0900

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
-class User < ApplicationRecord 
-     
+class User < ActiveRecord::Base
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+    length: { minimum: 3, maximum: 25 }
 end
